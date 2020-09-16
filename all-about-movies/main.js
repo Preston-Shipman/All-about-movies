2a89d803cd3df43045347945c87ab1c8b931361c
Preston Shipman <pshipman2000@gmail.com>
2020-09-16 01:29:29 -0700

    work in progress

--- all-about-movies/main.js
+++ all-about-movies/main.js
@@ -3,4 +3,6 @@ $(document).ready(initializeApp);
 function initializeApp(){
     var getMovies = new MovieResult();
     getMovies.addEventHandler();
+    $('.toast').toast('show')
+    $('.toast').toast(data - delay = 5000)
 }
